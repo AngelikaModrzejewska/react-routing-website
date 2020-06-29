0ba078cf6f4d2696301b7b9014a95cba724eacad
Angelika Modrzejewska <ang.modrzejewska@gmail.com>
2020-06-29 17:53:19 +0200

    Updates

---
+++ precache-manifest.19209d95740ea3c29bcddc79046d046d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "67432acf46d71e6ebc06",
+    "url": "/react-routing-website/static/css/main.3a42609e.chunk.css"
+  },
+  {
+    "revision": "67432acf46d71e6ebc06",
+    "url": "/react-routing-website/static/js/main.67432acf.chunk.js"
+  },
+  {
+    "revision": "20e822d43101d3331062",
+    "url": "/react-routing-website/static/js/runtime~main.20e822d4.js"
+  },
+  {
+    "revision": "047d52afe050e4dd728a",
+    "url": "/react-routing-website/static/js/2.047d52af.chunk.js"
+  },
+  {
+    "revision": "5bc55e4b414decddef011f43fdb3ac79",
+    "url": "/react-routing-website/static/media/header1.5bc55e4b.jpg"
+  },
+  {
+    "revision": "672b5635ff99e5dddf82989ef0737929",
+    "url": "/react-routing-website/static/media/header2.672b5635.jpg"
+  },
+  {
+    "revision": "d196ad454f1d7c90d95b83dfeef0c3fa",
+    "url": "/react-routing-website/static/media/header3.d196ad45.jpg"
+  },
+  {
+    "revision": "56d75a7a8aab4d32d59a91611d620514",
+    "url": "/react-routing-website/index.html"
+  }
+];
